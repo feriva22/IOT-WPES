14171de5656091af65d5f05c615d373a75144e9d
feriva22 <fericodeva69@gmail.com>
2020-07-12 05:37:36 +0700

    new bosku

--- index.php
+++ index.php
@@ -1,37 +1,118 @@
-<!DOCTYPE html>
+
+<!doctype html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Data Sensor</title>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="assets/favicon.ico">
 
-    <!-- SCRIPT CSS PLUGIN-->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <title>Wpes Landing Page</title>
 
-</head>
-<body>
-    <main role="main">
-        <section class="jumbotron text-center">
-            <div class="container">
-              <h1 class="jumbotron-heading">WPES (Water Purification Electrocoagulation System)</h1>
-			  <p>WPES adalah alat yang digunakan untuk memonitoring dan menjernihkan air secara otomatis</p>
-            </div>
-        </section>
+    <link rel="canonical" href="index.html">
+
+    <!-- Bootstrap core CSS -->
+    <link href="https://getbootstrap.com/docs/4.1/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="https://getbootstrap.com/docs/4.1/examples/album/album.css" rel="stylesheet">
+  </head>
+
+  <body>
+
+    <header>
+      <div class="collapse bg-dark" id="navbarHeader">
         <div class="container">
-            <div class="row text-center" id="all-device">
-                <p>Klik Link Berikut untuk Mendapatkan Aplikasi WPES</p>       
-				<a href="https://wpes-app.herokuapp.com/" class="btn btn-lg btn-primary">Klik Disini</a>
+          <div class="row">
+            <div class="col-sm-8 col-md-7 py-4">
+              <h4 class="text-white">Tentang</h4>
+              <p class="text-muted">Wpes dibuat pada awalnya untuk memberikan solusi dalam kebersihan air dengan cara metode elektrokoagulasi, diharapkan dengan metode ini dapat tetap menjaga kualitas air rumahan, wpes saat ini masih dalam tahap uji coba, wpes dibuat oleh 3 mahasiswa UISI (Ferico Deno Vandra, Alferdo Doni Fadlillah, Adhitama Ihza Pangestu).</p>
             </div>
+            <div class="col-sm-4 offset-md-1 py-4">
+              <h4 class="text-white">Contact</h4>
+              <ul class="list-unstyled">
+                <li><a href="https://github.com/feriva22/IOT-WPES" class="text-white">Source Code on Github</a></li>
+                <li><a href="mailto:fericodeva69@gmail.com" class="text-white">Email me</a></li>
+              </ul>
+            </div>
+          </div>
         </div>
+      </div>
+      <div class="navbar navbar-dark bg-dark shadow-sm">
+        <div class="container d-flex justify-content-between">
+          <a href="index.html#" class="navbar-brand d-flex align-items-center">
+            
+            <strong>WPES</strong>
+          </a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarHeader" aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+        </div>
+      </div>
+    </header>
+
+    <main role="main">
+
+      <section class="jumbotron text-center">
+        <div class="container">
+          <h1 class="jumbotron-heading">WPES</h1>
+          <p class="lead text-muted">Water Purification Electrocoagulation System</p>
+          <p>
+            <a href="https://wpes-app.herokuapp.com/" class="btn btn-primary my-2">Get APP</a>
+          </p>
+        </div>
+      </section>
+
+      <div class="album py-5 bg-light">
+        <div class="container">
+
+          <div class="row">
+            <div class="col-md-4">
+              <div class="card mb-4 shadow-sm">
+                <img class="card-img-top" src="assets/ilustrasi-meter-air-pdam.jpg" alt="Card image cap">
+                <div class="card-body">
+                  <p class="card-text">Wpes memberikan kemudahan dalam memonitoring penggunaan air rumah.</p>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-4">
+              <div class="card mb-4 shadow-sm">
+                <img class="card-img-top" src="assets/AIR-BERSIH.png" alt="Card image cap">
+                <div class="card-body">
+                  <p class="card-text">Wpes membantu pemilik rumah dalam memastikan air tetap bersih bagi keluarga.</p>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-4">
+              <div class="card mb-4 shadow-sm">
+                <img class="card-img-top" src="assets/image-wpes.png" alt="Card image cap">
+                <div class="card-body">
+                  <p class="card-text">Wpes mudah diakses dimanapun dan kapanpun.</p>
+                </div>
+              </div>
+            </div>
+
+            
+          </div>
+        </div>
+      </div>
+
     </main>
-    <!-- SCRIPT JS LOAD-->
-    <script src="assets/jquery-3.4.1.min.js"></script>
-    <script>
-       
-    </script>
-</body>
+
+    <footer class="text-muted">
+      <div class="container">
+        <p>Copyright &copy; feriva22!</p>
+      </div>
+    </footer>
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script>window.jQuery || document.write('<script src="https://getbootstrap.com/docs/4.1/assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
+    <script src="https://getbootstrap.com/docs/4.1/assets/js/vendor/popper.min.js"></script>
+    <script src="https://getbootstrap.com/docs/4.1/dist/js/bootstrap.min.js"></script>
+    <script src="https://getbootstrap.com/docs/4.1/assets/js/vendor/holder.min.js"></script>
+  </body>
 </html>
